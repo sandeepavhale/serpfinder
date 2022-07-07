8d64cff4641c1d750ced6a12158c963509dd9cdc
sandeepavhale <sandeep.avhale143@gmail.com>
2022-07-07 22:56:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__app__)
+app = Flask(__name__)
 
-@route.route('/')
-def hello_word()
-    return 'hello word'
+@app.route('/')
+def hello_world():
+   return 'Hello World'
 
-if __name__ == '__main__';
+if __name__ == '__main__':
     app.run()
